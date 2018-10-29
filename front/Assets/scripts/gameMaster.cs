b0b615b48535e56456c2bfabb2239e303f426919
Sebastien ALIBERT <salibert@student.42.fr>
2018-10-29 18:08:06 +0100

    [ADD] board

---
+++ front/Assets/scripts/gameMaster.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class gameMaster : MonoBehaviour
+{
+    public struct nodes {
+        public float x, y;
+        public int player;
+        public bool captered;
+    }
+}
---
+++ front/Assets/scripts/goban.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class goban : MonoBehaviour
+{
+    public gameMaster.nodes[][] board;
+    void Start()
+    {
+        board = new gameMaster.nodes[19][];
+        Transform line = null;
+        Transform lines = transform.Find("lines").transform;
+        intersections scriptInter = transform.Find("intersections").GetComponent<intersections>();
+        for (int i = 0; i < lines.childCount; i++)
+        {
+            line = lines.GetChild(i).transform;
+            if (line.rotation.y == 0) {
+                board[i] = findIntersections(line);
+                scriptInter.createIntersection(board[i].Length, board[i]);
+            }
+        }
+    }
+
+    private gameMaster.nodes[] findIntersections(Transform line) {
+        Vector3 fwd = transform.TransformDirection(transform.forward);
+        List<gameMaster.nodes> nodes = new List<gameMaster.nodes>();
+        List<RaycastHit> hitsAll = new List<RaycastHit>();
+
+        hitsAll.AddRange(Physics.RaycastAll(line.position, fwd, 1000));
+        hitsAll.AddRange(Physics.RaycastAll(line.position, fwd * -1, 1000));
+        // hitsAll.Sort(el => {  })
+        hitsAll.ForEach(el => {
+            nodes.Add(new gameMaster.nodes() {
+                x = line.position.x,
+                y = el.transform.position.z,
+                player = 0,
+                captered = false });
+        });
+        return nodes.ToArray();
+    }
+
+
+}
