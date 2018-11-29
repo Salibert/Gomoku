0db779289e081b1c807cb89d9fbc089815ec4430
Sebastien ALIBERT <salibert@student.42.fr>
2018-11-29 12:09:13 +0100

    [ADD] menu

---
+++ front/Assets/scripts/mainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainMenu : MonoBehaviour {
+
+	static public int modeGame;
+	public void PlayGame1VS1() {
+		modeGame = 2;
+		// DontDestroyOnLoad(transform.gameObject);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void PlayGameAI() {
+		modeGame = 1;
+		// DontDestroyOnLoad(transform.gameObject);
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame() {
+		Debug.Log("QUIT");
+		Application.Quit();
+	}
+}
---
+++ front/Assets/scripts/pauseMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class pauseMenu : MonoBehaviour {
+
+	public static bool GameIsPaused = false;
+	public GameObject pauseMenuUI;
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (GameIsPaused) {
+				Resume();
+			} else {
+				Pause();
+			}
+		}
+	}
+
+
+	public void Resume() {
+		GameIsPaused = false;
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+	}
+
+	public void Pause() {
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+	public void LoadMenu() {
+		SceneManager.LoadScene("Menu");
+	}
+	public void QuitGame() {
+		Debug.Log("QUIT");
+		Application.Quit();
+	}
+}
