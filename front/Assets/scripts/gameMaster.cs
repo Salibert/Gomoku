4d33cd8881e4b91b3dd9b9f5017c9418c6e1ccaf
Sebastien ALIBERT <salibert@student.42.fr>
2018-12-05 15:16:57 +0100

    [FIX]

--- front/Assets/scripts/gameMaster.cs
+++ front/Assets/scripts/gameMaster.cs
@@ -71,6 +71,7 @@ public class gameMaster : MonoBehaviour
         try {
             GomokuBuffer.StonePlayed reply = await Client.PlayedAsync(
                 new GomokuBuffer.StonePlayed(){ CurrentPlayerMove=node.Clone(), GameID=GameID  });
+            await GetCheckRules(reply.CurrentPlayerMove, reply.CurrentPlayerMove.Player);
             Transform stone = goban.GetStone(reply.CurrentPlayerMove);
             stone.transform.GetComponent<stone>().SetStone();
         } catch (Exception e) {
@@ -79,32 +80,34 @@ public class gameMaster : MonoBehaviour
         }
     }
 
+    private void updateCapture(GomokuBuffer.CheckRulesResponse reply) {
+        if (reply.NbStonedCaptured != 0) {
+            CurrentPlayer.SetScore(CurrentPlayer.GetScore() + reply.NbStonedCaptured);
+            int index;
+            GomokuBuffer.Node elementNode;
+            foreach(GomokuBuffer.Node capturedStone in reply.Captured) {
+                index = 0;
+                foreach(stone el in goban.board) {
+                    elementNode = el.GetNode();
+                    if (elementNode.X == capturedStone.X && elementNode.Y == capturedStone.Y) {
+                        el.Reset();
+                        break;
+                    }
+                    index++;
+                }
+                goban.board.RemoveAt(index);
+            }
+        }
+    }
     async public Task<bool> GetCheckRules(GomokuBuffer.Node node, int player) {
         try {
             node.Player = player;
             GomokuBuffer.CheckRulesResponse reply = await Client.CheckRulesAsync(
                 new GomokuBuffer.StonePlayed(){ CurrentPlayerMove=node.Clone(), GameID=GameID });
             Debug.Log("List capture "+ reply.Captured);
-            if (reply.NbStonedCaptured != 0) {
-                CurrentPlayer.SetScore(CurrentPlayer.GetScore() + reply.NbStonedCaptured);
-                int index;
-                GomokuBuffer.Node elementNode;
-                foreach(GomokuBuffer.Node capturedStone in reply.Captured) {
-                    index = 0;
-                    foreach(stone el in goban.board) {
-                        elementNode = el.GetNode();
-                        if (elementNode.X == capturedStone.X && elementNode.Y == capturedStone.Y) {
-                            el.Reset();
-                            break;
-                        }
-                        index++;
-                    }
-                    goban.board.RemoveAt(index);
-                }
-            }
+            updateCapture(reply);
             if (reply.PartyFinish == true) {
                 Debug.Log("GG SALE PUTE !!!" + reply.WinIs);
-
             }
             return reply.IsPossible;
         } catch (Exception e) {
