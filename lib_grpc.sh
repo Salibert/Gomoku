d5929966185d055d0a23ddca5611d6ff59d1e035
Hamza MADAD <hmadad@e1r2p5.42.fr>
2019-01-04 17:27:48 +0100

    [ADD] Script for importe lib for game

---
+++ lib_grpc.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [ "$#" -ne 1 ]; then
+    echo "lib_grpc: only 1 argument is required."
+    echo "usage: ./lib_grpc unity_game"
+else
+    FILE=$1
+
+    if mkdir -p $FILE/Contents/Frameworks/MonoEmbedRuntime/osx; then
+        if cp bin_tools/tools/macosx_x64/libgrpc_csharp_ext.bundle $FILE/Contents/Frameworks/MonoEmbedRuntime/osx/ ; then
+            echo "Lib successfully imported"
+        fi
+    fi
+    
+fi
